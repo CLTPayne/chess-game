62d6f0548a14f16c08d268937be5578188c699ab
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2019-02-20 22:25:35 +0000

    Snazzy solution from Instagram comments

---
+++ instagramChessboard.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+const isEven = num => num % 2 === 0;
+
+const arrOf = size => [...Array(size).keys()];
+
+const blackOrWhiteIf = predicate => arg => predicate(arg) ? "⬜️" : "⬛️";
+
+const boardRow = blackOrWhiteIf(isEven);
+
+const displace = row => col => col + (row % 2);
+
+const chessReducer = (acc, row, _i, arr) => {
+  return acc + arr.map(displace(row)).map(boardRow).join('') + "\n"
+}
+
+const chessboard = (size = 8) => arrOf(size).reduce(chessReducer, '');
+
+console.log(chessboard())

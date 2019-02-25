3aad27d3c7d259d52278de16c925b9087b6d5dbd
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2019-02-25 22:03:34 +0000

    Check if board is chess size

--- renderChessboard.js
+++ renderChessboard.js
@@ -50,8 +50,10 @@ const currentBoard = generateBoard(
 const currentPosition = getPosition(currentBoard, 7, 7);
 
 currentPosition.color = "😀";
+//
+// console.log(renderBoard(currentBoard));
 
-console.log(renderBoard(currentBoard));
+console.log(currentBoard)
 
 // Next Steps:
 // Function setupChessBoard - takes a board, checks the size of the board (8) is correct for chess
@@ -59,3 +61,9 @@ console.log(renderBoard(currentBoard));
 // The render function can be dumb but then render what ever emoji is in the piece value
 // Make the render function useable in browser.
 // Persistent state for knowing the past state of the board, and the player1.
+
+const isChessBoard = (board) => {
+  return board.length === 64 ? true : false;
+}
+
+console.log(isChessBoard(currentBoard));

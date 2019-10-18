4bd79a68cbc2b6a0413c6af5a0b976a20ec1f62e
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2019-10-19 00:25:08 +0100

    Stop eslint conflict with local prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,44 +1,25 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"es6": true,
+	env: {
+		browser: true,
+		es6: true,
 		"jest/globals": true
 	},
-	"extends": "eslint:recommended",
-	"plugins": ["prettier", "jest"],
-	"globals": {
-		"Atomics": "readonly",
-		"SharedArrayBuffer": "readonly"
+	extends: "eslint:recommended",
+	plugins: ["prettier", "jest"],
+	globals: {
+		Atomics: "readonly",
+		SharedArrayBuffer: "readonly"
 	},
-	"parserOptions": {
-		"ecmaVersion": 2018,
-		"sourceType": "module"
+	parserOptions: {
+		ecmaVersion: 2018,
+		sourceType: "module"
 	},
-	"rules": {
-		"indent": [
-			"error",
-			"tab"
-		],
-		"linebreak-style": [
-			"error",
-			"unix"
-		],
-		"quotes": [
-			"error",
-			"double"
-		],
-		"semi": [
-			"error",
-			"never"
-		],
-		"complexity": [
-			"error", 5
-		],
-		"max-depth": [
-			"warn", 4 
-		],
-		"max-statements": [
-			"warn", 10 
-		]
+	rules: {
+		indent: ["error", "tab"],
+		"linebreak-style": ["error", "unix"],
+		quotes: ["error", "double"],
+		complexity: ["error", 5],
+		"max-depth": ["warn", 4],
+		"max-statements": ["warn", 10]
 	}
-}
+};
